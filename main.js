3ae04daae06b1dcda2a9f06ab3fa702a52538716
surze <angpaono1@hotmail.com>
2021-03-03 13:21:29 +0700

    [ADD/EDIT_CODE/DELETE] add_preview FINISH edit_code FINISH del_old_css&index

---
+++ main.js
@@ -0,0 +1,64 @@
+const constraints = {
+    username: {
+        presence: true,
+        
+    },
+    fullname: {
+        presence: true,
+    },
+    pwd: {
+        presence: true,
+        length:{
+            minimum : 6,
+            message: "must be at least 4 digits"
+        }
+    },
+    email: {
+        presence: true,
+        email: true
+    },
+    title: {
+        presence: true,
+    },
+    
+}
+const app =Vue.createApp({
+    data() {
+        return {
+            username: null,
+            email: null,
+            pwd: null,
+            fullname: null,
+            title: null,
+            errors: null,
+            image: "images/2.jpg",
+            showfullname: null,
+            showtitle: null,
+            showemail: null,
+        }
+    },
+    methods: {
+        checkForm(e){
+            this.errors = validate({username: this.username,
+                                    email: this.email,
+                                    pwd: this.pwd,
+                                    fullname: this.fullname,
+                                    title: this.title},
+                                    constraints)
+            if(this.errors){
+                e.preventDefault();
+                
+            }
+            else{
+                this.updateinfomation();
+                alert("Your profile is updated successfully.");
+            }
+        },
+        updateinfomation(){
+            this.showfullname = this.fullname;
+            this.showtitle = "-"+" "+this.title;
+            this.showemail = this.email;
+        },
+    }
+})
+app.mount('#app')
